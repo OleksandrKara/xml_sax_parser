22d597adf17611a059445c03b98fd44b64029b07
Kara Oleksandr <Oleksandr_Kara@epam.com>
2014-05-08 14:13:45 +0300

    Ma 1 commit

---
+++ XmlSaxParser/src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Oleksandr_Kara
+ * Date: 5/8/14
+ * Time: 2:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+}
